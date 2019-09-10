d360f8a1074aff3292297b8531305f943445366a
Meero Harootunian <meero.harootunian@gmail.com>
2019-09-09 21:55:11 -0700

    added logic.js with 4 characters

---
+++ assets/js/logic.js
@@ -0,0 +1,23 @@
+defender = {
+    name: "Defender",
+    hp: 200,
+    attack: 5
+}
+
+attacker = {
+    name: "Attacker",
+    hp: 120,
+    attack: 8
+}
+
+ranger = {
+    name: "Ranger",
+    hp: 150,
+    attack: 6
+}
+
+rocketeer = {
+    name: "Rocketeer",
+    hp: 100,
+    attack: 13
+}
