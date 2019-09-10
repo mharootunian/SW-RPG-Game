6ca95d17bbc837ae52229eec8e8c64447c1da6e8
Meero Harootunian <meero.harootunian@gmail.com>
2019-09-09 22:28:22 -0700

    added images and changed some character names

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,23 +1,24 @@
-defender = {
+var defender = {
     name: "Defender",
     hp: 200,
     attack: 5
 }
 
-attacker = {
+var attacker = {
     name: "Attacker",
     hp: 120,
     attack: 8
 }
 
-ranger = {
+var ranger = {
     name: "Ranger",
     hp: 150,
-    attack: 6
+    attack: 11
 }
 
-rocketeer = {
-    name: "Rocketeer",
-    hp: 100,
-    attack: 13
+var rocketeer = {
+    name: "Covert",
+    hp: 80,
+    attack: 15
 }
+
